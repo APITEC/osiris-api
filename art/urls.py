91a67fa0a77162ee35b8fcaade244234cf513f4b
desdenova420 <8091957+desdenova420@users.noreply.github.com>
2020-07-10 10:46:09 -0600

    Added view actions.

--- art/urls.py
+++ art/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from art import views
 
 urlpatterns = [
-    path('piece/', views.Piece.as_view()),
-    path('transaction/', views.Transaction.as_view()),
+    path('piece/', views.Piece.as_view({'get': 'list', 'post': 'create', 'patch': 'partial_update'})),
+    path('transaction/', views.Transaction.as_view({'get': 'list', 'post': 'create', 'patch': 'partial_update'})),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('', views.User.as_view()),
+    path('', views.User.as_view({'get': 'list', 'post': 'create', 'patch': 'partial_update'})),
 ]
