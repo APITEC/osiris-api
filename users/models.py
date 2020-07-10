31b520a7727b7056358699c78928a729b43a1ea1
desdenova420 <8091957+desdenova420@users.noreply.github.com>
2020-07-10 11:35:45 -0600

    Added email to User.

--- users/models.py
+++ users/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    email = models.EmailField()
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    image = models.ImageField()
+    image = models.ImageField(blank=True, null=True)
     government_id = models.CharField(max_length=100)
     stamp = models.CharField(max_length=50)
